ab71127247f9b89349603790ae6c482c0cf7d94e
Dan Hiester <hiesterd@bloomington.in.gov>
2015-02-11 11:10:53 -0500

    Updates #22 Checkbox helper first iteration

--- templates/html/helpers/Input.php
+++ templates/html/helpers/Input.php
@@ -12,7 +12,6 @@ use Blossom\Classes\Helper;
 
 class Input extends Helper
 {
-
     public function text($id, $label, $value = '', $type = 'text', $required = false, $inputAttributesArray = [])
     {
         $extraAttributes = '';
@@ -22,11 +21,25 @@ class Input extends Helper
             $extraAttributes .= "$attribute=\"$attrValue\" ";
         }
         echo <<<EOT
+        <dl class="input-field">
+            <dt><label for="$id">$required$label</label></dt>
+            <dd><input id="$id" name="$id" value="$value" type="$type" $extraAttributes/></dd>
+        </dl>
 
-            <dl class="input-field">
-                <dt><label for="$id">{$required}$label</label></dt>
-                <dd><input id="$id" name="$id" value="$value" type="$type" $extraAttributes/></dd>
-            </dl>
+EOT;
+    }
+
+    public function checkbox($groupLabel, $items = []) {
+        foreach ($items as $item) {
+            $checked = $item['checked'] ? 'checked="checked"' : '';
+            $dds .= "<dd><label><input type=\"checkbox\" value=\"{$item['value']}\" $checked><span>{$item['label']}</label>";
+        }
+        echo <<<EOT
+        <dl class="input-field mod-checkbox">
+            <dt>$groupLabel</dt>
+            $dds
+        </dl>
+
 EOT;
     }
 }
