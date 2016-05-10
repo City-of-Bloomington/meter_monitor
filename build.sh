d960feb32b87f14fb5085d4a1b1a7108f59b7dcb
inghamn <inghamn@bloomington.in.gov>
2016-05-10 16:23:27 -0400

    Updated build script to build language and css

--- build.sh
+++ build.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-RELEASE_NAME=meter_monitor
+APPNAME=meter_monitor
 DIR=`pwd`
 BUILD=$DIR/build
-DIST=$DIR/dist
 
 if [ ! -d $BUILD ]
-    then mkdir $BUILD
+	then mkdir $BUILD
 fi
 
-if [ ! -d $DIST ]
-    then mkdir $DIST
-fi
+# Compile the Lanague files
+cd $DIR/language
+./build_lang.sh
+cd $DIR
 
-# Compile the SCSS
+# Compile the SASS
 cd $DIR/public/css/local
 ./build_css.sh
 cd $DIR
 
 # The PHP code does not need to actually build anything.
 # Just copy all the files into the build
-rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
-
-# Create a distribution tarball of the build
-tar czvf $DIST/$RELEASE_NAME.tar.gz --transform=s/build/$RELEASE_NAME/ $BUILD
+rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/$APPNAME
+cd $BUILD
+tar czvf $APPNAME.tar.gz $APPNAME
